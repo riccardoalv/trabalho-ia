113051e5b0038a08fd11967f4bc715008d0dfad6
Ricardo Alves da Silva <ricardcpu@gmail.com>
2024-03-17 12:20:06 -0400

    add first example using images

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "NixOS environment";
+
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; };
+
+  outputs = { self, nixpkgs, }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+
+    in
+    {
+      devShell.${system} = with pkgs;
+
+        mkShell rec {
+          packages = [
+            python311
+            pipenv
+            nodejs
+            python311Packages.transformers
+            python311Packages.torch
+            (pkgs.python311Packages.opencv4.override { enableGtk2 = true; })
+          ];
+        };
+    };
+}
--- main.py
+++ main.py
@@ -1,4 +1,43 @@
+import cv2
+import concurrent.futures
 from transformers import pipeline
+from PIL import Image
 
-classifier = pipeline("image-classification", model="dima806/facial_emotions_image_detection")
-print(classifier("./download.jpeg")[0]["label"])
+# Função para classificação de emoções em uma imagem
+def classify_emotions(image):
+    classifier = pipeline("image-classification", model="jayanta/google-vit-base-patch16-224-cartoon-emotion-detection")
+    return classifier(image)
+
+# Função para processamento de cada frame em segundo plano
+def process_frame_in_background(frame):
+    # Converter frames para imagens Pillow
+    image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+    # Classificar emoções na imagem
+    emotions = classify_emotions(image)
+    print(emotions)
+
+# Inicializar a captura de vídeo
+cv2.namedWindow("preview")
+vc = cv2.VideoCapture(0)
+
+if vc.isOpened(): # Tentar obter o primeiro frame
+    rval, frame = vc.read()
+else:
+    rval = False
+
+# Iniciar o pool de execução para processar quadros em segundo plano
+executor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
+
+while rval:
+    cv2.imshow("preview", frame)
+    rval, frame = vc.read()
+    key = cv2.waitKey(20)
+    if key == 27: # Sair com ESC
+        break
+    # Processar o quadro em segundo plano
+    executor.submit(process_frame_in_background, frame)
+
+# Liberar recursos
+vc.release()
+cv2.destroyWindow("preview")
+
